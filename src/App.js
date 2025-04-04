3288d658847058570a1d265dde0b19986a2cfc1b
s-zhaojo <56804773+s-zhaojo@users.noreply.github.com>
2025-04-04 11:51:54 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -133,12 +133,11 @@ const MapComponent = () => {
         </div>
         <div className="card">
           <h3>Total Cost</h3>
-          <p>distInMiles * carbonEmissions.car</p>
-          <p></p>
+          <p>${totalCost.toFixed(2)}</p>
         </div>
         <div className="card">
           <h3>Total CO2 Emissions</h3>
-          <p> distInMiles * transportationCosts.car</p>
+          <p>{totalEmissions.toFixed(2)} kg CO2</p>
         </div>
     </div> 
     
