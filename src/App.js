b4edad9b0e5abdd03c2097e139e6d7e1fb21e485
s-zhaojo <56804773+s-zhaojo@users.noreply.github.com>
2025-05-20 16:28:45 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -131,9 +131,17 @@ const MapComponent = () => {
         <div className="sidebar">
            <div className="card">
             <img src={forgeLogo} alt="BALLOONS" width="50%" height="50%"/>
-            <a href="https://student.naviance.com/main">
-              <button>{isLoggedIn ? 'Log Out' : 'Log In'}</button>
-            </a>
+            <button
+              onClick={() => {
+              if (!isLoggedIn) {
+                window.location.href = 'https://student.naviance.com/main';
+              } else {
+                handleLogin(); // or handleLogout depending on your logic
+              }
+             }}
+          >
+            {isLoggedIn ? 'Log Out' : 'Log In'}
+        </button>
           </div>
           <div className="card">
             <h3>Total Distance</h3>
