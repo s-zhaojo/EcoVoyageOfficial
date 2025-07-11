e6026c1c8f291d6ae6520470238a2a5896c6e16e
sunshinetown <88341723+cubeiceman@users.noreply.github.com>
2025-07-10 17:31:39 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -65,6 +65,20 @@ const MapComponent = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+        window.removeEventListener('resize', handleWindowSizeChange);
+    }
+  }, []);
+
+  const isMobile = width <= 768;
+
   const handleLoginClick = () => {
     if (isLoggedIn) {
       // Logout
